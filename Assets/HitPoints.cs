b5bfc8f822b73f2984e8ec7c071e06beb81833e0
ahls <ahls@uci.edu>
2021-03-31 15:18:43 -0700

    added the health funciton basic

---
+++ Assets/HitPoints.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitPoints : MonoBehaviour
+{
+    [SerializeField] private float maxHP;
+    public float currentHP { set; get; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #region setups
+    public void init()
+    {
+        currentHP = maxHP;
+    }
+    public void init(float newHealth)
+    {
+        maxHP = newHealth;
+        init();
+    }
+    #endregion
+
+
+
+    public void takeDamage(float dmg)
+    {
+        currentHP -= dmg;
+
+        if(currentHP <= 0)
+        {
+            onDeath();
+        }
+    }
+    private void onDeath()
+    {
+        gameObject.SetActive(false);
+    }
+}
