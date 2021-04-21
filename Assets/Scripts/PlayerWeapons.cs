033d9444c2666c964ad987297c73a771a406a037
ahls <ahls@uci.edu>
2021-04-21 13:16:52 -0700

    adjusted the values of the weapon almost done with the ammo system and display

--- Assets/Scripts/PlayerWeapons.cs
+++ Assets/Scripts/PlayerWeapons.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Animations.Rigging;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class PlayerWeapons : MonoBehaviour
 {
@@ -22,8 +23,10 @@ public class PlayerWeapons : MonoBehaviour
     public weaponData _weaponData { set; get; }
     public Dictionary<int,weaponData> weapons = new Dictionary<int, weaponData>();
     private int currentWeaponIndex = 3;
+    private float reloadingTimer;
+    private bool isReloading = false;
     [SerializeField] GameObject KnifePrefab;
-
+    [SerializeField] Text loadedAmmo, totalAmmo;
 
     //riggings
     [SerializeField] Transform leftHand;
@@ -50,20 +53,15 @@ public class PlayerWeapons : MonoBehaviour
         fireTimer -= Time.deltaTime;
         if (_weaponData != null)
         {
-            if (Input.GetMouseButton(0)) // if left clicked
-            {
-                if (fireTimer <= 0)
-                {
-                    fire();
-                }
-            }
+            fireCheck();
         }
         swap();
         aimDownCheck();
         dropWeaponCheck();
+        reloadCheck();
     }
 
-
+    #region Equipping related
     public void equipWeapon(int index)
     {
         if(!weapons.ContainsKey(index))
@@ -86,6 +84,7 @@ public class PlayerWeapons : MonoBehaviour
         fireflare = _weaponData.fireLocation.GetComponent<ParticleSystem>();
         audioSource = firelocation.parent.GetComponent<AudioSource>();
         audioSource.clip = _weaponData.shotSound;
+        updateAmmoInfo();
     }
     private void swap()
     {
@@ -109,58 +108,27 @@ public class PlayerWeapons : MonoBehaviour
         }
     }
 
-    private void fire()
-    {
-        fireTimer = 60 / _weaponData.fireRate; // cooldown is (60seconds / RPM)
-        RaycastHit aimHit;
-        if (Physics.Raycast(cam.transform.position+cam.transform.forward*2,cam.transform.forward,out aimHit,_weaponData.range))
-        {
-            RaycastHit hit;
-            for (int i = 0; i < _weaponData.numPelletes; i++)
-            {
-                Vector3 spreadOffset = new Vector3(Random.Range(-_weaponData.bulletSpread, _weaponData.bulletSpread),
-                                                   Random.Range(-_weaponData.bulletSpread, _weaponData.bulletSpread),
-                                                   Random.Range(-_weaponData.bulletSpread, _weaponData.bulletSpread));
-                if (Physics.Raycast(firelocation.position, aimHit.point - firelocation.position + spreadOffset, out hit, _weaponData.range))
-                {
-                    GameObject tempImpact = Instantiate(ImpactPrefab, hit.point, Quaternion.LookRotation(transform.position - hit.point));
-                    Destroy(tempImpact, 0.3f);
-                    HitPoints HP = hit.transform.GetComponent<HitPoints>();
-                    if (HP != null)
-                    {
-                        HP.takeDamage(_weaponData.damage);
-                    }
-                }
-            }
-        }
-        fireflare.Play();
-        audioSource.PlayOneShot(audioSource.clip,audioSource.volume);
-        _playermovement.AddRecoil(_weaponData.RecoilX_min, _weaponData.RecoilY_min, _weaponData.RecoilX_max, _weaponData.RecoilY_max);
-        
-    }
-
-
     public bool pickupWeapon(weaponData pickedWeapon)
     {
-        if(weapons.Count <2)
+        if (weapons.Count < 2)
         {// if there is a empty spot for a new weapon
-            if(!weapons.ContainsKey(1))
+            if (!weapons.ContainsKey(1))
             {//if 0th slot is empty
                 weapons[1] = pickedWeapon;
-                if(weapons.Count == 2)
+                if (weapons.Count == 2)
                 {
                     equipWeapon(1);
                 }
             }
-            else if(!weapons.ContainsKey(2))
+            else if (!weapons.ContainsKey(2))
             {//if 1st slot is empty
-                weapons[2] = pickedWeapon; 
+                weapons[2] = pickedWeapon;
                 if (weapons.Count == 2)
                 {
                     equipWeapon(2);
                 }
             }
-            if(weapons.Count == 3)
+            if (weapons.Count == 3)
             {
                 slotFull = true;
             }
@@ -168,17 +136,21 @@ public class PlayerWeapons : MonoBehaviour
         }
         return false;
     }
+
+    /// <summary>
+    /// funciton that is called on Update to check for dropping waepon
+    /// </summary>
     private void dropWeaponCheck()
     {
-        if(Input.GetKeyDown(KeyCode.B))
+        if (Input.GetKeyDown(KeyCode.B))
         {
-            if(currentWeaponIndex ==3)
+            if (currentWeaponIndex == 3)
             {//you are holding a melee weapon, which cannot be dropped
                 return;
             }
-            else if(currentWeaponIndex ==1)
+            else if (currentWeaponIndex == 1)
             {
-                if(weapons.ContainsKey(2))
+                if (weapons.ContainsKey(2))
                 {
                     equipWeapon(2);
                 }
@@ -189,7 +161,7 @@ public class PlayerWeapons : MonoBehaviour
                 spawnWeapon(weapons[1]);
                 weapons.Remove(1);
             }
-            else if(currentWeaponIndex == 2)
+            else if (currentWeaponIndex == 2)
             {
                 if (weapons.ContainsKey(1))
                 {
@@ -204,12 +176,100 @@ public class PlayerWeapons : MonoBehaviour
             }
         }
     }
-
+    /// <summary>
+    /// helper funciton that creates the weapon on the world
+    /// </summary>
+    /// <param name="droppingWeapon"></param>
     private void spawnWeapon(weaponData droppingWeapon)
     {
 
     }
 
+    #endregion
+
+
+
+    #region Shooting related
+    private void fireCheck()
+    {
+        if (Input.GetMouseButton(0)) // if left clicked
+        {
+            if (!isReloading)
+            {
+                if (_weaponData.loadedAmmo > 0)
+                {
+                    fire();
+                }
+                else if (_weaponData.currentAmmo > 0)
+                {
+                    reloadingTimer = Time.time + _weaponData.reloadTime;
+                    isReloading = true;
+                }
+            }
+        }
+    }
+    private void fire()
+    {
+        if (fireTimer > 0) return;
+        fireTimer = 60 / _weaponData.fireRate; // cooldown is (60seconds / RPM)
+        for (int i = 0; i < _weaponData.numPelletes; i++)
+        {
+            Quaternion bulletSpread = Quaternion.Euler(new Vector3(
+                                                   Random.Range(-_weaponData.bulletSpread, _weaponData.bulletSpread),
+                                                   Random.Range(-_weaponData.bulletSpread, _weaponData.bulletSpread),
+                                                   0));
+            RaycastHit aimHit;
+            if (Physics.Raycast(cam.transform.position + cam.transform.forward * 2, bulletSpread * cam.transform.forward, out aimHit, _weaponData.range))
+            {
+                RaycastHit hit;
+
+                if (Physics.Raycast(firelocation.position, aimHit.point - firelocation.position, out hit, _weaponData.range))
+                {
+                    GameObject tempImpact = Instantiate(ImpactPrefab, hit.point, Quaternion.LookRotation(transform.position - hit.point));
+                    Destroy(tempImpact, 0.3f);
+                    HitPoints HP = hit.transform.GetComponent<HitPoints>();
+                    if (HP != null)
+                    {
+                        HP.takeDamage(_weaponData.damage);
+                    }
+                }
+            }
+        }
+        _weaponData.loadedAmmo--;
+        loadedAmmo.text = _weaponData.loadedAmmo.ToString();
+        fireflare.Play();
+        audioSource.PlayOneShot(audioSource.clip,audioSource.volume);
+        _playermovement.AddRecoil(_weaponData.RecoilX_min, _weaponData.RecoilY_min, _weaponData.RecoilX_max, _weaponData.RecoilY_max);
+        
+    }
+
+    private void reloadCheck()
+    {
+
+        if (Input.GetKeyDown(KeyCode.R) && 
+            _weaponData.currentAmmo > 0 &&
+            _weaponData.loadedAmmo != _weaponData.magazineSize &&
+            !isReloading)
+        {
+            reloadingTimer = Time.time + _weaponData.reloadTime;
+            isReloading = true;
+        }
+        if(isReloading)
+        {
+            if(Time.time > reloadingTimer)
+            {
+                reloadWeapon();
+            }
+        }
+    }
+    private void reloadWeapon()
+    {
+        int newMag = Mathf.Min(_weaponData.magazineSize, _weaponData.currentAmmo+_weaponData.loadedAmmo);
+        _weaponData.currentAmmo -= newMag - _weaponData.loadedAmmo;
+        _weaponData.loadedAmmo = newMag;
+        updateAmmoInfo();
+        isReloading = false;
+    }
     private void aimDownCheck()
     {
         if (Input.GetMouseButton(1))
@@ -221,4 +281,12 @@ public class PlayerWeapons : MonoBehaviour
             aimRig.weight = Mathf.Lerp(aimRig.weight, 0, lerpSpeed);
         }
     }
+    #endregion
+
+    private void updateAmmoInfo()
+    {
+        loadedAmmo.text = _weaponData.loadedAmmo.ToString();
+        totalAmmo.text = _weaponData.currentAmmo.ToString();
+    }
+
 }
--- Assets/Scripts/weaponData.cs
+++ Assets/Scripts/weaponData.cs
@@ -9,9 +9,13 @@ public class weaponData : MonoBehaviour
     public float fireRate;
     public float range;
     public float bulletSpread;
+    public float reloadTime;
     public float damage;
     public int maxAmmo;
+    public int currentAmmo;
     public int magazineSize;
+    public int loadedAmmo;
+   
 
     public AudioClip shotSound;
     public float RecoilY_min, RecoilY_max;
