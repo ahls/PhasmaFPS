1b61ae57b1bc5e1b7d6cd2aa45679507730af027
ahls <ahls@uci.edu>
2021-05-03 12:21:42 -0700

    working on the title and lobby

--- Assets/PUN_LobbyManager.cs
+++ Assets/PUN_LobbyManager.cs
@@ -7,12 +7,22 @@ public class PUN_LobbyManager : MonoBehaviour
 {
     [SerializeField] GameObject _playerPanelPrefab;
     [SerializeField] Transform _playersPanel;
+    private PhotonView _pv;
     // Start is called before the first frame update
     void Start()
     {
+        _pv = GetComponent<PhotonView>();
         GameObject newPanel =  PhotonNetwork.Instantiate(Path.Combine("Prefabs","Networking",_playerPanelPrefab.name),Vector3.zero,Quaternion.identity,0);
-        newPanel.transform.SetParent(_playersPanel);
+        _pv.RPC("AddToPanel", RpcTarget.MasterClient, newPanel);
+        if (!PhotonNetwork.IsMasterClient)
+        {
+            _playersPanel.gameObject.SetActive(false);
+        }
     }
 
-
+    [PunRPC]
+    private void AddToPanel(GameObject newPanel)
+    {
+        newPanel.transform.SetParent(_playersPanel);
+    }
 }
--- Assets/PlayerPanelBehaviour.cs
+++ Assets/PlayerPanelBehaviour.cs
@@ -34,7 +34,7 @@ public class PlayerPanelBehaviour : MonoBehaviour
         {
             PhotonNetwork.LoadLevel("Arena");
         }
-
-
     }
+
+
 }
